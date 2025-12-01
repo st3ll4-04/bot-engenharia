dee090a65a2f70891a54bbc861063cacd07a45de
st3ll4-04 <masettistella4@gmail.com>
2025-12-01 12:01:10 -0300

    automacao-atualizado

--- main.py
+++ main.py
@@ -49,14 +49,53 @@ with SB() as sb:
 
     #3. Automação de Compra
 
-    #Adicionar Mochila ao carrinho
+    # Adicionar todos os itens ao carrinho
     sb.click("#add-to-cart-sauce-labs-backpack")
-    # Ir para o carrinho de comptras
-    sb.click("shopping_cart_container")
-    # Iniciar o processo de checkout
-    sb.click("checkout")
-    
-    # Para conseguir ver o que apareceu  na tela, vamos esperar 8 segundos
-    sb.sleep(8)
+    sb.click("#add-to-cart-sauce-labs-bike-light")
+    sb.click("#add-to-cart-sauce-labs-bolt-t-shirt")
+    sb.click("#add-to-cart-sauce-labs-fleece-jacket")
+    sb.click("#add-to-cart-sauce-labs-onesie")
+    sb.click('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]')
 
+    # Usar wait para aguardar para aguardar o botao carregar
+    sb.wait_for_element(".shopping_cart_link")
+    # Ir para o carrinho de comptras
+    sb.click(".shopping_cart_link")
     
+    # Usar wait para carregar o botao de checkout
+    sb.wait_for_element("#checkout")
+    # Iniciar o processo de checkout
+    sb.click("#checkout")
+
+    # Preencher o formulário
+    sb.type("#first-name", "Trainee")
+    sb.type("#last-name", "PiJunior")
+    sb.type("#postal-code", "31270-901")
+
+    # Usar wait para carregar o botao continue
+    sb.wait_for_element("#continue")
+    # Continuar para a tela de resumo
+    sb.click("#continue")
+
+    # Usar wait para carregar o botao finish
+    sb.wait_for_element("#finish")
+    # Finalizar compra
+    sb.click("#finish")
+
+    # Raspar informações da compra
+    #CONFERIR!!!
+    # Raspar as informacoes da compra
+    pagamento = sb.get_text('.summary_value_label[data-test="payment-info-value"]')
+    entrega = sb.get_text('.summary_value_label[data-test="shipping-info-value"]')
+    valor_total = sb.get_text('.summary_total_label[data-test="total-label"]')
+
+    print("\nInformações da compra:")
+    print("Meio de pagamento:", pagamento)
+    print("Forma de entrega:", entrega)
+    print("Valor total:", valor_total)
+
+    # Verificar se a compra foi concluída
+    confirmacao = sb.get_text(".complete-header")
+
+    print("\nMensagem de confirmação:")
+    print(confirmacao)
